9426cfc38b435ffaefb65c7b5b27c6c1d39ffb74
TG <trygod.nwakwasi@decagon.dev>
2022-11-29 00:19:23 +0100

    first commit

---
+++ src/main/java/com/musalasoftdroneservice/MusalaSoftDroneServiceApplication.java
@@ -0,0 +1,13 @@
+package com.musalasoftdroneservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusalaSoftDroneServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusalaSoftDroneServiceApplication.class, args);
+    }
+
+}
