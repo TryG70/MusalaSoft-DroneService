e5832e862f13b0147108da4e9479b4dcf62deec7
TG <trygod.nwakwasi@decagon.dev>
2022-12-01 23:42:10 +0100

    Refactored the methods and added a new endpoint: offLoadDrone

--- src/main/java/com/musalasoftdroneservice/controller/DroneController.java
+++ src/main/java/com/musalasoftdroneservice/controller/DroneController.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import javax.validation.Valid;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -25,7 +25,7 @@ public class DroneController {
 
     @Operation(summary = "Register a new drone")
     @PostMapping(value = "/register/drone")
-    public ResponseEntity<?> registerDrone(@RequestBody DroneDto droneDto) {
+    public ResponseEntity<?> registerDrone(@Valid @RequestBody DroneDto droneDto) {
         return new ResponseEntity<>(droneService.registerDrone(droneDto), CREATED);
     }
 
@@ -35,22 +35,27 @@ public class DroneController {
         return new ResponseEntity<>(droneService.loadDrone(serialNumber, code), OK);
     }
 
-    @Operation(summary = "Get the medication items in the drone at a given time")
-    @GetMapping(value = "/drone/medication-items/{serialNumber}/{time}")
-    public ResponseEntity<?> getDroneMedicationItems(@PathVariable(value = "serialNumber") String serialNumber, @PathVariable(value = "time") LocalDateTime time) {
-        return new ResponseEntity<>(droneService.getDroneMedicationItems(serialNumber, time), FOUND);
+    @Operation(summary = "Get drone loaded medication items")
+    @GetMapping(value = "/drone/medication-items/{serialNumber}")
+    public ResponseEntity<?> getDroneMedicationItems(@PathVariable(value = "serialNumber") String serialNumber) {
+        return new ResponseEntity<>(droneService.getDroneMedicationItems(serialNumber), OK);
     }
 
     @Operation(summary = "Get all available drones(Drones with the state of the drone as Idle and Battery level greater than 25%)")
     @GetMapping(value = "/available-drone")
     public ResponseEntity<?> getAvailableDrones(){
-        return new ResponseEntity<>(droneService.getAvailableDrones(), FOUND);
+        return new ResponseEntity<>(droneService.getAvailableDrones(), OK);
     }
 
-    @Operation(summary = "Get a Drones battery level")
+    @Operation(summary = "Get drone battery level")
     @GetMapping(value = "/drone/battery-level/{serialNumber}")
     public ResponseEntity<?> getDroneBatteryLevel(@PathVariable(value = "serialNumber") String serialNumber) {
-        return new ResponseEntity<>(droneService.getDroneBatteryLevel(serialNumber), FOUND);
+        return new ResponseEntity<>(droneService.getDroneBatteryLevel(serialNumber), OK);
     }
 
+    @Operation(summary = "Offload drone medications")
+    @GetMapping(value = "/offload-drone/{serialNumber}")
+    public ResponseEntity<?> offloadDrone(@PathVariable(value = "serialNumber") String serialNumber) {
+        return new ResponseEntity<>(droneService.offloadDrone(serialNumber), OK);
+    }
 }
