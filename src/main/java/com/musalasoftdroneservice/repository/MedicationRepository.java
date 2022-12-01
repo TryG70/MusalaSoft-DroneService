4f468cd0e39bde4139927a261d7fcbb9ca645b2e
TG <trygod.nwakwasi@decagon.dev>
2022-12-01 23:47:20 +0100

    removed a redundant custom query

--- src/main/java/com/musalasoftdroneservice/repository/MedicationRepository.java
+++ src/main/java/com/musalasoftdroneservice/repository/MedicationRepository.java
@@ -4,15 +4,11 @@ import com.musalasoftdroneservice.entity.Medication;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MedicationRepository extends JpaRepository<Medication, Long> {
 
     Optional<Medication> findByCode(String code);
-
-    List<Medication> findAllByDrone_IdAndUpdatedAt(long id, LocalDateTime date);
 
 }
