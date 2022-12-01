9160e172ad0f668ad44af2314cd6b209fbc676ff
TG <trygod.nwakwasi@decagon.dev>
2022-12-01 23:46:49 +0100

    added validation at the dto class level

--- src/main/java/com/musalasoftdroneservice/dto/MedicationDto.java
+++ src/main/java/com/musalasoftdroneservice/dto/MedicationDto.java
@@ -6,20 +6,25 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+
 @Builder
 @Data
 public class MedicationDto {
+
 
     @Pattern(regexp = "(^[a-zA-Z0-9-_])$", message = "letters, numbers, hyphen and underscore allowed")
     @NotNull(message = "Name of medication is required")
     private String name;
 
+
     @NotNull(message = "Weight of medication is required")
     private Double weight;
+
 
     @Pattern(regexp = "(^[A-Z0-9_])$", message = "upper case letters, numbers and underscore allowed")
     @NotNull(message = "Code of medication is required")
     private String code;
+
 
     private String image;
 }
